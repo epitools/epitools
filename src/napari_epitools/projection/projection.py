a51891543bd20925318da8d8f4918166ef3bd40b
Daniel Matthews <d.matthews@ucl.ac.uk>
2022-08-02 16:55:14 +0100

    Refactor projection_widget

--- src/napari_epitools/projection/projection.py
+++ src/napari_epitools/projection/projection.py
@@ -5,10 +5,24 @@ from napari.types import ImageData
 from skimage.filters import gaussian
 from typing_extensions import Annotated
 
-from napari_epitools.analysis._gridfit import gridfit
+from napari_epitools.projection._gridfit import gridfit
 
 
 def _smooth(img, smoothing_radius=0.3):
+    """Gaussian smoothing of each z plane in the image stack
+
+    Parameters
+    ----------
+    img : ndarray
+        The input image stack
+    smoothing_radius : float, optional
+        Standard deviation passed to gaussian function, by default 0.3
+
+    Returns
+    -------
+    ndarray
+        The smoothed image stack
+    """
     zsize = img.shape[0]
     smoothed = np.zeros(img.shape)
     for z in range(zsize):
@@ -18,16 +32,58 @@ def _smooth(img, smoothing_radius=0.3):
 
 
 def _interpolate(depthmap, imsize, smoothness):
-    # plt.figure()
-    # plt.imshow(depthmap)
+    """Interpolate the z coordinate map using gridfit
+
+    Parameters
+    ----------
+    depthmap : ndarray
+        z coordinate map
+    imsize : tuple
+        Input image dimensions
+    smoothness : float
+        How much to smooth the interpolation - lower numbers indicate more smoothing
+
+    Returns
+    -------
+    ndarray
+        Interpolated z coordinates
+    """
     indices = np.nonzero(depthmap)
     vals = depthmap[indices]
 
     xnodes, ynodes = imsize[2], imsize[1]
     X, Y = np.meshgrid(np.arange(0, xnodes, 1), np.arange(0, ynodes, 1))
 
-    # return X, Y, griddata(indices, vals, (X, Y), method="nearest", fill_value=0.0)
     return gridfit(indices[1], indices[0], vals, xnodes, ynodes, smoothness)
+
+
+def _calculate_projected_image(imstack, z_interpolation):
+    """Create the projected image from the non-zero elements
+    of the interpolated z coordinates.
+
+    Parameters
+    ----------
+    imstack : ndarray
+        The input image z stack
+    z_interpolation : ndarray
+        Output from gridfit function
+
+    Returns
+    -------
+    ndarray
+        The final projected image
+    """
+
+    # make a container for the projected image
+    imsize = imstack.shape
+    projected_image = np.zeros((imsize[1], imsize[2]), dtype=imstack.dtype)
+
+    # take the non-zero elements of the interpolation and round
+    mask = z_interpolation > 0
+    z_coordinates = np.round(z_interpolation[mask]).astype("int")
+    projected_image[mask] = imstack[z_coordinates, mask]
+
+    return projected_image
 
 
 @magic_factory(pbar={"visible": False, "max": 0, "label": "working..."})
@@ -83,46 +139,27 @@ def projection_widget(
         # assumed to be the surface of interest
         mask = confidencemap > confthres
         depthmap2 = depthmap * mask
-        xg1, yg1, zg1 = _interpolate(depthmap2, imsize, surface_smoothness_1)
+        _, _, zg1 = _interpolate(depthmap2, imsize, surface_smoothness_1)
 
         # given the hight locations of the surface (zg1) compute the difference
         # towards the 1st quartile location (depthmap2), ignore the rest (==0);
         # the result reflects the distance (abs) between estimate and points.
         depthmap3 = np.abs(zg1 - depthmap2.astype("float64"))
-        depthmap3[np.ma.where(depthmap2 == 0)] = 0
+        depthmap3[np.where(depthmap2 == 0)] = 0
 
-        # only keep points which are relatively close to our first estimate,
-        # i.e. below the threshold. TIP: if the first estimate is too detailed(~=smooth)
-        # the points from the peripodial membrane will not be eliminated since
-        # the surface approximated them well. Increase the smoothness to prevent this.
+        # only keep points which are relatively close to our first estimate
         mask = depthmap3 < cut_off_distance
         depthmap4 = depthmap2 * mask
-
-        # TIP: depthmap4 should only contain signal of interest at this point.
 
         # --- 2nd iteration -
         # compute a better more detailed estimate with the filtered list (depthmap4)
         # this is to make sure that the highest intensity points will be
         # selected from the correct surface (The coarse grained estimate could
         # potentially approximate the origin of the point to another plane)
-        xg2, yg2, zg2 = _interpolate(depthmap4, imsize, surface_smoothness_2)
+        _, _, zg2 = _interpolate(depthmap4, imsize, surface_smoothness_2)
 
-        # ----- creating projected image from interpolated surface estimation ------
-
-        projected_image = np.zeros((imsize[1], imsize[2]), dtype=imstack.dtype)
-        z_origin_map = np.zeros((imsize[1], imsize[2]), dtype="uint8")
-
-        # zg2_mask = zg2 > 0
-        # z_coordinates = z_origin_map * zg2_mask
-        # z_origin_map[zg2_mask] = np.round(zg2[zg2_mask])
-        # projected_image[zg2_mask] = imstack[z_coordinates, zg2_mask]
-
-        for y in range(imsize[1]):
-            for x in range(imsize[2]):
-                if zg2[y, x] > 0:
-                    z_coordinate = int(round(zg2[y, x]))
-                    z_origin_map[y, x] = z_coordinate
-                    projected_image[y, x] = imstack[z_coordinate, y, x]
+        # creating projected image from interpolated surface estimation
+        projected_image = _calculate_projected_image(imstack, zg2)
 
         return projected_image
 
