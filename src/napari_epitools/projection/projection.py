c551589a646dd8deedac9b6dd129f0de5001fafc
Daniel Matthews <d.matthews@ucl.ac.uk>
2022-08-04 15:36:58 +0100

    Move code for projection calculation to separate function

--- src/napari_epitools/projection/projection.py
+++ src/napari_epitools/projection/projection.py
@@ -84,6 +84,67 @@ def _calculate_projected_image(imstack, z_interpolation):
     return projected_image
 
 
+def calculate_projection(
+    input_image,
+    smoothing_radius,
+    surface_smoothness_1,
+    surface_smoothness_2,
+    cut_off_distance,
+) -> np.ndarray:
+    """Z projection using image interpolation.
+
+    Parameters
+    ----------
+    imstack : ndarray
+        numpy ndarray representation of 3D image stack
+    smoothing_radius : float
+        kernel radius for gaussian blur to apply before estimating the surface
+    [0.1 - 5]
+    surface_smoothness_1: int
+        Surface smoothness for 1st gridFit(c) estimation, the smaller the smoother
+        [30 - 100]
+    surface_smoothness_2: int
+        Surface smoothness for 3nd gridFit(c) estimation, the smaller the smoother
+        [20 - 50]
+    cut_off_distance : int
+        Cutoff distance in z-planes from the 1st estimated surface [1 - 3]
+    """
+    imsize = input_image.shape
+    I1 = _smooth(input_image, smoothing_radius)
+
+    vm1 = I1.max(axis=0)
+    depthmap = I1.argmax(axis=0)
+
+    confidencemap = imsize[0] * vm1 / sum(I1, 0)
+    c = confidencemap[:]
+    confthres = np.median(c[c > np.median(c)])
+
+    # keep only the brightest surface points (intensity in 1 quartile)
+    # assumed to be the surface of interest
+    mask = confidencemap > confthres
+    depthmap2 = depthmap * mask
+    zg1 = _interpolate(depthmap2, imsize, surface_smoothness_1)
+
+    # given the hight locations of the surface (zg1) compute the difference
+    # towards the 1st quartile location (depthmap2), ignore the rest (==0);
+    # the result reflects the distance (abs) between estimate and points.
+    depthmap3 = np.abs(zg1 - depthmap2.astype("float64"))
+    depthmap3[np.where(depthmap2 == 0)] = 0
+
+    # only keep points which are relatively close to our first estimate
+    mask = depthmap3 < cut_off_distance
+    depthmap4 = depthmap2 * mask
+
+    # --- 2nd iteration -
+    # compute a better more detailed estimate with the filtered list (depthmap4)
+    # this is to make sure that the highest intensity points will be
+    # selected from the correct surface (The coarse grained estimate could
+    # potentially approximate the origin of the point to another plane)
+    zg2 = _interpolate(depthmap4, imsize, surface_smoothness_2)
+
+    return _calculate_projected_image(input_image, zg2)
+
+
 @magic_factory(pbar={"visible": False, "max": 0, "label": "working..."})
 def projection_widget(
     pbar: widgets.ProgressBar,
@@ -105,7 +166,7 @@ def projection_widget(
     ----------
     pbar: magicgui.widget
         Progressbar widget
-    imstack : ndarray
+    input_image : ndarray
         numpy ndarray representation of 3D image stack
     smoothing_radius : float
         kernel radius for gaussian blur to apply before estimating the surface
@@ -121,49 +182,18 @@ def projection_widget(
     """
 
     @thread_worker(connect={"returned": pbar.hide})
-    def calculate_projection() -> ImageData:
-        # image dimensions are z, y, x
+    def projection() -> ImageData:
         if input_image is None:
             pbar.hide()
             raise ValueError("Load an image first")
 
-        imsize = input_image.shape
-        I1 = _smooth(input_image, smoothing_radius)
-
-        vm1 = I1.max(axis=0)
-        depthmap = I1.argmax(axis=0)
-
-        confidencemap = imsize[0] * vm1 / sum(I1, 0)
-        c = confidencemap[:]
-        confthres = np.median(c[c > np.median(c)])
-
-        # keep only the brightest surface points (intensity in 1 quartile)
-        # assumed to be the surface of interest
-        mask = confidencemap > confthres
-        depthmap2 = depthmap * mask
-        zg1 = _interpolate(depthmap2, imsize, surface_smoothness_1)
-
-        # given the hight locations of the surface (zg1) compute the difference
-        # towards the 1st quartile location (depthmap2), ignore the rest (==0);
-        # the result reflects the distance (abs) between estimate and points.
-        depthmap3 = np.abs(zg1 - depthmap2.astype("float64"))
-        depthmap3[np.where(depthmap2 == 0)] = 0
-
-        # only keep points which are relatively close to our first estimate
-        mask = depthmap3 < cut_off_distance
-        depthmap4 = depthmap2 * mask
-
-        # --- 2nd iteration -
-        # compute a better more detailed estimate with the filtered list (depthmap4)
-        # this is to make sure that the highest intensity points will be
-        # selected from the correct surface (The coarse grained estimate could
-        # potentially approximate the origin of the point to another plane)
-        zg2 = _interpolate(depthmap4, imsize, surface_smoothness_2)
-
-        # creating projected image from interpolated surface estimation
-        projected_image = _calculate_projected_image(input_image, zg2)
-
-        return projected_image
+        return calculate_projection(
+            input_image,
+            smoothing_radius,
+            surface_smoothness_1,
+            surface_smoothness_2,
+            cut_off_distance,
+        )
 
     pbar.show()
-    return calculate_projection()
+    return projection()
