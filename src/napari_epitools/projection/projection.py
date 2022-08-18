07da8a10b8a3b6d7ed9e98bf79a79f7eb5551580
Daniel Matthews <d.matthews@ucl.ac.uk>
2022-08-18 14:05:41 +0100

    Better variable naming

--- src/napari_epitools/projection/projection.py
+++ src/napari_epitools/projection/projection.py
@@ -31,33 +31,36 @@ def _smooth(
 
 
 def _interpolate(
-    depthmap: npt.NDArray[np.float64],
-    imsize: npt.NDArray[np.float64],
+    max_indices: npt.NDArray[np.float64],
+    x_size: int,
+    y_size: int,
     smoothness: int,
 ) -> npt.NDArray[np.float64]:
     """Interpolate the z coordinate map using gridfit
 
     Args:
-        depthmap:
+        max_indices:
             z coordinate map.
-        imsize:
-            Input image dimensions.
+        x_size:
+            Image size in x dimension (number of columns)
+        y_size:
+            Image size in y dimension (number of rows)
         smoothness:
             How much to smooth the interpolation.
 
     Returns:
         Interpolated z coordinates.
     """
-    indices = np.nonzero(depthmap)
-    vals = depthmap[indices].astype(np.float64)
-    xnodes, ynodes = imsize[2], imsize[1]
+    indices = np.nonzero(max_indices)
+    vals = max_indices[indices].astype(np.float64)
+    xnodes, ynodes = x_size, y_size
     X, Y = np.meshgrid(np.arange(xnodes), np.arange(ynodes))
     interp_vals = griddata(indices, vals, (Y, X), method="nearest")
     return gaussian(interp_vals, sigma=smoothness)
 
 
 def _calculate_projected_image(
-    imstack: npt.NDArray[np.float64], z_interpolation: npt.NDArray[np.float64]
+    imstack: npt.NDArray[np.float64], z_interp: npt.NDArray[np.float64]
 ) -> npt.NDArray[np.float64]:
     """Create the projected image from the non-zero elements
     of the interpolated z coordinates.
@@ -65,7 +68,7 @@ def _calculate_projected_image(
     Args:
         imstack:
             The input image z stack.
-        z_interpolation:
+        z_interp:
             Output from griddata function.
 
     Returns:
@@ -77,8 +80,8 @@ def _calculate_projected_image(
     projected_image = np.zeros((imsize[-2:]), dtype=imstack.dtype)
 
     # take the non-zero elements of the interpolation and round
-    mask = (z_interpolation > 0) & (z_interpolation < imsize[0] - 1)
-    z_coordinates = np.round(z_interpolation[mask]).astype(np.int64)
+    mask = (z_interp > 0) & (z_interp < imsize[0] - 1)
+    z_coordinates = np.round(z_interp[mask]).astype(np.int64)
     projected_image[mask] = imstack[z_coordinates, mask]
     return projected_image
 
@@ -107,13 +110,13 @@ def calculate_projection(
     Returns:
         Stack projected onto a single plane.
     """
-    imsize = input_image.shape
-    I1 = _smooth(input_image, smoothing_radius)
+    z_planes, y_size, x_size = input_image.shape
+    smoothed_imstack = _smooth(input_image, smoothing_radius)
 
-    vm1 = I1.max(axis=0)
-    depthmap = I1.argmax(axis=0)
+    max_intensity = smoothed_imstack.max(axis=0)
+    max_indices = smoothed_imstack.argmax(axis=0)
 
-    confidencemap = imsize[0] * vm1 / sum(I1, 0)
+    confidencemap = z_planes * max_intensity / sum(smoothed_imstack, 0)
     confthres = np.median(
         confidencemap[confidencemap > np.median(confidencemap)]
     )
@@ -121,26 +124,32 @@ def calculate_projection(
     # keep only the brightest surface points (intensity in 1 quartile)
     # assumed to be the surface of interest
     mask = confidencemap > confthres
-    depthmap2 = depthmap * mask
-    zg1 = _interpolate(depthmap2, imsize, surface_smoothness_1)
+    max_indices_confthres = max_indices * mask
+    z_interp = _interpolate(
+        max_indices_confthres, x_size, y_size, surface_smoothness_1
+    )
 
-    # given the hight locations of the surface (zg1) compute the difference
-    # towards the 1st quartile location (depthmap2), ignore the rest (==0);
+    # given the hight locations of the surface (z_interp) compute the difference
+    # towards the 1st quartile location (max_indices_confthres), ignore the rest (==0);
     # the result reflects the distance (abs) between estimate and points.
-    depthmap3 = np.abs(zg1 - depthmap2.astype("float64"))
-    depthmap3[np.where(depthmap2 == 0)] = 0
+    max_indices_diff = np.abs(
+        z_interp - max_indices_confthres.astype("float64")
+    )
+    max_indices_diff[np.where(max_indices_confthres == 0)] = 0
 
     # only keep points which are relatively close to our first estimate
-    mask = depthmap3 < cut_off_distance
-    depthmap4 = depthmap2 * mask
+    mask = max_indices_diff < cut_off_distance
+    max_indices_cut = max_indices_confthres * mask
 
     # --- 2nd iteration -
-    # compute a better more detailed estimate with the filtered list (depthmap4)
+    # compute a better more detailed estimate with the filtered list (max_indices_cut)
     # this is to make sure that the highest intensity points will be
     # selected from the correct surface (The coarse grained estimate could
     # potentially approximate the origin of the point to another plane)
-    zg2 = _interpolate(depthmap4, imsize, surface_smoothness_2)
-    return _calculate_projected_image(input_image, zg2)
+    z_interp = _interpolate(
+        max_indices_cut, x_size, y_size, surface_smoothness_2
+    )
+    return _calculate_projected_image(input_image, z_interp)
 
 
 @magic_factory(pbar={"visible": False, "max": 0, "label": "working..."})
