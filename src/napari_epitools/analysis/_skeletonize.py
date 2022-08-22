0584d7676f43212e787e33d356bc5773d3cc2893
Daniel Matthews <d.matthews@ucl.ac.uk>
2022-08-22 15:32:01 +0100

    Add skeletonisation

---
+++ src/napari_epitools/analysis/_skeletonize.py
@@ -0,0 +1,60 @@
+import numpy as np
+from numpy import ndarray
+
+
+def _get_lines(cell_labels):
+    gradient = np.gradient(cell_labels)
+    return (cell_labels > 0.0) & (
+        np.square(gradient[0]) + np.square(gradient[1]) > 0.0
+    ).astype(int)
+
+
+def skeletonize(cell_labels: ndarray) -> ndarray:
+    """Skeletonize a labelled image by calculating the gradient.
+
+    Parameters
+    ----------
+    cell_labels : np.ndarray
+        Labelled (possibly time series) image
+
+    Returns
+    -------
+    np.ndarray
+        The skeletonized labelled image
+
+    Raises
+    ------
+    ValueError
+        If the input data has more than 3 dimensions.
+    """
+    if cell_labels.ndim == 2:
+        return _get_lines(cell_labels)
+
+    elif cell_labels.ndim == 3:
+        time_points = cell_labels.shape[0]
+        cell_outlines = []
+        for t in range(time_points):
+            frame = cell_labels[t]
+            cell_outlines.append(_get_lines(frame))
+
+        return np.stack(cell_outlines)
+
+    elif cell_labels.ndim > 3:
+        raise ValueError(
+            "Input data should be a time series with a single z plane"
+        )
+
+
+if __name__ == "__main__":
+    import h5py
+
+    f = h5py.File("sample_data/8bitDataset/Benchmark/SegResults.mat")
+    cell_labels = f.get("CLabels")
+
+    skels = skeletonize(cell_labels)
+    print(f"{skels.shape=}")
+    from matplotlib import pyplot as plt
+
+    plt.figure()
+    plt.imshow(skels[5])
+    plt.show()
