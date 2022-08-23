729d5037de54ccc6131fc234a51a7c8891838548
Daniel Matthews <d.matthews@ucl.ac.uk>
2022-08-23 16:22:46 +0100

    Use magicgui direct api

--- src/napari_epitools/_widget.py
+++ src/napari_epitools/_widget.py
@@ -1,9 +1,6 @@
-from typing import List
-
-from magicgui import magic_factory, widgets
-from napari.qt.threading import FunctionWorker, thread_worker
-from napari.types import ImageData, LabelsData, LayerDataTuple
-from napari.utils.notifications import show_info
+from magicgui import widgets
+from napari import current_viewer
+from napari.layers import Image
 
 from napari_epitools.analysis import (
     calculate_projection,
@@ -16,152 +13,156 @@ SEED_SIZE = 3
 SEED_EDGE_COLOR = "red"
 SEED_FACE_COLOR = "red"
 
+# Defaults
+SMOOTHING_RADIUS = 0.2
+SMOOTHING_RADIUS_MIN = 0.0
+SMOOTHING_RADIUS_MAX = 2.0
+SMOOTHING_RADIUS_STEP = 0.1
 
-@magic_factory(
-    pbar={"visible": False, "max": 0, "label": "working..."},
-    smoothing_radius={
-        "widget_type": "FloatSlider",
-        "min": 0.0,
-        "max": 2.0,
-        "step": 0.1,
-    },
-    surface_smoothness_1={
-        "widget_type": "Slider",
-        "min": 0,
-        "max": 10,
-        "step": 1,
-    },
-    surface_smoothness_2={
-        "widget_type": "Slider",
-        "min": 0,
-        "max": 10,
-        "step": 1,
-    },
-    cut_off_distance={
-        "widget_type": "Slider",
-        "min": 0,
-        "max": 5,
-        "step": 1,
-    },
-)
-def projection_widget(
-    pbar: widgets.ProgressBar,
-    input_image: ImageData,
-    smoothing_radius: float = 0.2,
-    surface_smoothness_1: int = 5,
-    surface_smoothness_2: int = 5,
-    cut_off_distance: int = 2,
-) -> FunctionWorker[LayerDataTuple]:
-    """Z projection using image interpolation.
-    Args:
-        pbar:
-            Progressbar widget
-        input_image:
-            Numpy ndarray representation of 3D image stack
-        smoothing_radius:
-            Kernel radius for gaussian blur to apply before estimating the surface.
-        surface_smoothness_1:
-            Surface smoothness for 1st griddata estimation, larger means smoother.
-        surface_smoothness_2:
-            Surface smoothness for 3nd griddata estimation, larger means smoother.
-        cut_off_distance:
-            Cutoff distance in z-planes from the 1st estimated surface.
-    Raises:
-        ValueError: When no image is loaded.
-    Returns:
-        Projected image as napari Image layer.
+SURFACE_SMOOTHNESS_1 = 5
+SURFACE_SMOOTHNESS_1_MIN = 0
+SURFACE_SMOOTHNESS_1_MAX = 10
+SURFACE_SMOOTHNESS_1_STEP = 1
+
+SURFACE_SMOOTHNESS_2 = 5
+SURFACE_SMOOTHNESS_2_MIN = 0
+SURFACE_SMOOTHNESS_2_MAX = 10
+SURFACE_SMOOTHNESS_2_STEP = 1
+
+CUT_OFF_DISTANCE = 2
+CUT_OFF_DISTANCE_MIN = 0
+CUT_OFF_DISTANCE_MAX = 5
+CUT_OFF_DISTANCE_STEP = 1
+
+SPOT_SIGMA = 3
+SPOT_SIGMA_MIN = 0
+SPOT_SIGMA_MAX = 20
+SPOT_SIGMA_STEP = 0.1
+
+OUTLINE_SIGMA = 0
+OUTLINE_SIGMA_MIN = 0
+OUTLINE_SIGMA_MAX = 20
+OUTLINE_SIGMA_STEP = 0.1
+
+THRESHOLD = 20
+THRESHOLD_MIN = 0
+THRESHOLD_MAX = 255
+THRESHOLD_STEP = 1
+
+
+def epitools_widget() -> widgets.Container:
     """
-    if input_image is None:
-        pbar.hide()
-        show_info("Load an image first")
-        return
+    Widgets for epitools
+    """
+    # Layout for projection
+    input_image = widgets.create_widget(annotation=Image, label="Image:")
+    smoothing_radius = widgets.FloatSlider(
+        name="smoothing_radius",
+        value=SMOOTHING_RADIUS,
+        min=SMOOTHING_RADIUS_MIN,
+        max=SMOOTHING_RADIUS_MAX,
+        step=SMOOTHING_RADIUS_STEP,
+    )
+    surface_smoothness_1 = widgets.Slider(
+        name="surface_smoothness_1",
+        value=SURFACE_SMOOTHNESS_1,
+        min=SURFACE_SMOOTHNESS_1_MIN,
+        max=SURFACE_SMOOTHNESS_1_MAX,
+        step=SURFACE_SMOOTHNESS_1_STEP,
+    )
+    surface_smoothness_2 = widgets.Slider(
+        name="surface_smoothness_2",
+        value=SURFACE_SMOOTHNESS_2,
+        min=SURFACE_SMOOTHNESS_2_MIN,
+        max=SURFACE_SMOOTHNESS_2_MAX,
+        step=SURFACE_SMOOTHNESS_2_STEP,
+    )
+    cut_off_distance = widgets.Slider(
+        name="cutoff_distance",
+        value=CUT_OFF_DISTANCE,
+        min=CUT_OFF_DISTANCE_MIN,
+        max=CUT_OFF_DISTANCE_MAX,
+        step=CUT_OFF_DISTANCE_STEP,
+    )
+    run_proj_button = widgets.PushButton(
+        name="run_proj_button", label="Run Projection"
+    )
 
-    @thread_worker(connect={"returned": pbar.hide})
-    def run() -> LayerDataTuple:
-        proj = calculate_projection(
+    # Layout for segmentation
+    seg_image = widgets.create_widget(
+        annotation=Image, label="Projected Image:"
+    )
+    spot_sigma = widgets.FloatSlider(
+        name="spot_sigma",
+        value=SPOT_SIGMA,
+        min=SPOT_SIGMA_MIN,
+        max=SPOT_SIGMA_MAX,
+        step=SPOT_SIGMA_STEP,
+    )
+    outline_sigma = widgets.FloatSlider(
+        name="outline_sigma",
+        value=OUTLINE_SIGMA,
+        min=OUTLINE_SIGMA,
+        max=OUTLINE_SIGMA,
+        step=OUTLINE_SIGMA_STEP,
+    )
+    threshold = widgets.Slider(
+        name="threshold",
+        value=THRESHOLD,
+        min=THRESHOLD_MIN,
+        max=THRESHOLD_MAX,
+        step=THRESHOLD_STEP,
+    )
+
+    run_seg_button = widgets.PushButton(
+        name="run_seg_button", label="Run Segmentation"
+    )
+    widget = widgets.Container(
+        widgets=[
             input_image,
             smoothing_radius,
             surface_smoothness_1,
             surface_smoothness_2,
             cut_off_distance,
+            run_proj_button,
+            seg_image,
+            spot_sigma,
+            outline_sigma,
+            threshold,
+            run_seg_button,
+        ]
+    )
+    widget.viewer = current_viewer()
+
+    @widget.run_proj_button.clicked.connect
+    def run_projection() -> None:
+        proj = calculate_projection(
+            input_image.value.data,
+            smoothing_radius.value,
+            surface_smoothness_1.value,
+            surface_smoothness_2.value,
+            cut_off_distance.value,
         )
-        return (proj, {"name": "Projection"}, "image")
+        widget.viewer.add_image(proj, name="Projection")
 
-    pbar.show()
-    return run()
-
-
-@magic_factory(
-    pbar={"visible": False, "max": 0, "label": "working..."},
-    spot_sigma={
-        "widget_type": "FloatSlider",
-        "min": 0,
-        "max": 20,
-        "step": 0.1,
-    },
-    outline_sigma={
-        "widget_type": "FloatSlider",
-        "min": 0,
-        "max": 20,
-        "step": 0.1,
-    },
-    threshold={
-        "widget_type": "FloatSlider",
-        "min": 0,
-        "max": 100,
-        "step": 1,
-    },
-)
-def segmentation_widget(
-    pbar: widgets.ProgressBar,
-    input_image: ImageData,
-    spot_sigma: float = 3,
-    outline_sigma: float = 0,
-    threshold: float = 20,
-) -> FunctionWorker[List[LayerDataTuple]]:
-
-    if input_image is None or input_image.data.ndim > 2:
-        pbar.hide()
-        show_info("Load a 2D image first")
-        return
-
-    @thread_worker(connect={"returned": pbar.hide})
-    def run() -> List[LayerDataTuple]:
+    @widget.run_seg_button.clicked.connect
+    def run_segmentation() -> None:
         seeds, labels = thresholded_local_minima_seeded_watershed(
-            input_image, spot_sigma, outline_sigma, threshold
+            seg_image.value.data,
+            spot_sigma.value,
+            outline_sigma.value,
+            threshold.value,
         )
-        seeds_layer = (
-            seeds,
-            {
-                "name": "Seeds",
-                "size": SEED_SIZE,
-                "edge_color": SEED_EDGE_COLOR,
-                "face_color": SEED_FACE_COLOR,
-            },
-            "points",
-        )
-        labels_layer = (labels, {"name": "Segmentation"}, "labels")
-        return [seeds_layer, labels_layer]
-
-    pbar.show()
-    return run()
-
-
-@magic_factory
-def outlines_widget(
-    labels: LabelsData,
-) -> FunctionWorker[LayerDataTuple]:
-
-    if labels is None or labels.data.ndim > 2:
-        show_info("Outlines only works for labels")
-        return
-
-    def on_returned():
-        print("Finished")
-
-    @thread_worker(connect={"returned": on_returned})
-    def run() -> LayerDataTuple:
         lines = skeletonize(labels)
-        return (lines, {"name": "Skeletonize"}, "labels")
 
-    return run()
+        widget.viewer.add_points(
+            seeds,
+            name="Seed Points",
+            size=SEED_SIZE,
+            edge_color=SEED_EDGE_COLOR,
+            face_color=SEED_FACE_COLOR,
+        )
+        widget.viewer.add_labels(labels, name="Cells")
+        widget.viewer.add_labels(lines, name="Cell Outlines")
+
+    return widget
