cccf138ed2c8952bee295aea5352c3d4baa62d89
Paul Smith <paul.j.smith@ucl.ac.uk>
2023-04-14 15:10:44 +0100

    Add new sub-package for creating UI
    Use magicgui.widgets.create_widget for all widget creation, rathern than magic_factory

---
+++ src/napari_epitools/widgets/__init__.py
@@ -0,0 +1,3 @@
+from napari_epitools.widgets.cell_statistics import create_cell_statistics_widget
+from napari_epitools.widgets.projection import create_projection_widget
+from napari_epitools.widgets.segmentation import create_segmentation_widget
