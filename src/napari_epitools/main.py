2d8083074ee2e92551db86539bb088b53dcd1cf5
Paul Smith <paul.j.smith@ucl.ac.uk>
2023-04-14 15:05:08 +0100

    Remove widget creation from the main module

--- src/napari_epitools/main.py
+++ src/napari_epitools/main.py
@@ -7,201 +7,95 @@ import magicgui.widgets
 import matplotlib.pyplot as plt
 import napari.layers
 import napari.qt.threading
-import napari.types
 import numpy as np
 import numpy.typing as npt
 import pandas as pd
-from magicgui import magic_factory
 from magicgui.types import FileDialogMode
 from magicgui.widgets.bases import Widget
-from napari import current_viewer
-from napari.qt.threading import thread_worker
 
-from napari_epitools.analysis import (
-    calculate_cell_statistics,
-    calculate_projection,
-    calculate_segmentation,
-)
+import napari_epitools.analysis
+import napari_epitools.widgets
+
+__all__ = [
+    "create_projection_widget",
+    "create_segmentation_widget",
+    "create_cell_statistics_widget",
+]
 
 logger = logging.getLogger(__name__)
 
-# Rendering properties of seeds
-SEED_SIZE = 3
-SEED_EDGE_COLOR = "red"
-SEED_FACE_COLOR = "red"
+
+def create_projection_widget() -> magicgui.widgets.Container:
+    """Create a widget to project a 4d timeseries (TZYX) along the z dimension"""
+
+    projection_widget = napari_epitools.widgets.create_projection_widget()
+
+    # Project the timeseries when pressing the 'Run' button
+    projection_widget.run.changed.connect(
+        lambda: run_projection(
+            image=projection_widget.input_image.value,
+            smoothing_radius=projection_widget.smoothing_radius.value,
+            surface_smoothness_1=projection_widget.surface_smoothness_1.value,
+            surface_smoothness_2=projection_widget.surface_smoothness_2.value,
+            cutoff_distance=projection_widget.cutoff_distance.value,
+        ),
+    )
+
+    return projection_widget
 
 
-SMOOTHING_RADIUS = {
-    "widget_type": "FloatSlider",
-    "name": "smoothing_radius",
-    "min": 0.0,
-    "max": 10.0,
-    "step": 0.1,
-    "value": 1,
-}
+def run_projection(
+    image: napari.layers.Image,
+    smoothing_radius,
+    surface_smoothness_1,
+    surface_smoothness_2,
+    cutoff_distance,
+) -> None:
+    """Project a 4d timeseries along the z dimension"""
 
-SURFACE_SMOOTHNESS_1 = {
-    "widget_type": "Slider",
-    "name": "surface_smoothness_1",
-    "min": 0,
-    "max": 10,
-    "step": 1,
-    "value": 5,
-}
+    projected_data = napari_epitools.analysis.calculate_projection(
+        image.data,
+        smoothing_radius,
+        surface_smoothness_1,
+        surface_smoothness_2,
+        cutoff_distance,
+    )
 
-SURFACE_SMOOTHNESS_2 = {
-    "widget_type": "Slider",
-    "name": "surface_smoothness_2",
-    "min": 0,
-    "max": 10,
-    "step": 1,
-    "value": 5,
-}
-
-CUT_OFF_DISTANCE = {
-    "widget_type": "Slider",
-    "name": "cut_off_distance",
-    "min": 0,
-    "max": 5,
-    "step": 1,
-    "value": 3,
-}
-
-SPOT_SIGMA = {
-    "widget_type": "FloatSlider",
-    "name": "spot_sigma",
-    "min": 0,
-    "max": 20,
-    "step": 0.1,
-    "value": 10,
-}
-
-OUTLINE_SIGMA = {
-    "widget_type": "FloatSlider",
-    "name": "outline_sigma",
-    "min": 0,
-    "max": 20,
-    "step": 0.1,
-    "value": 3,
-}
-
-THRESHOLD = {
-    "widget_type": "FloatSlider",
-    "name": "threshold",
-    "min": 0,
-    "max": 100,
-    "step": 1,
-    "value": 3,
-}
-PROJECTION_LAYER_NAME = "Projection"
-SEEDS_LAYER_NAME = "Seeds"
-CELLS_LAYER_NAME = "Cells"
-WIDGET_NDIM = 3
+    viewer = napari.current_viewer()
+    viewer.add_image(
+        data=projected_data,
+        name="Projection",
+    )
 
 
-def _reset_axes(widget: Widget) -> None:
-    """Set the dimension sliders to `0`"""
-    axes = [0] if widget.viewer.dims.ndim == WIDGET_NDIM else [0, 1]
-    for axis in axes:
-        widget.viewer.dims.set_current_step(axis, 0)
+def create_segmentation_widget() -> magicgui.widgets.Container:
+    """Create a widget to segment a 3d (TYZ) timeseries"""
 
-
-def _add_layers(widget: Widget, layers: list[napari.types.LayerDataTuple]) -> None:
-    add_layer_func = {
-        "image": widget.viewer.add_image,
-        "labels": widget.viewer.add_labels,
-        "points": widget.viewer.add_points,
-    }
-    for layer in layers:
-        data, layer_data, layer_type = layer
-        try:
-            widget.viewer.layers[layer_data.get("name")].data = data
-        except KeyError:
-            add_layer_func[layer_type](data, **layer_data)
-            message = f"Adding a new layer {layer_data['name']}"
-            logger.debug(message)
-
-    _reset_axes(widget)
-
-
-@magic_factory(
-    smoothing_radius=SMOOTHING_RADIUS,
-    surface_smoothness_1=SURFACE_SMOOTHNESS_1,
-    surface_smoothness_2=SURFACE_SMOOTHNESS_2,
-    cut_off_distance=CUT_OFF_DISTANCE,
-)
-def projection_widget(
-    input_image: napari.types.ImageData,
-    smoothing_radius: float,
-    surface_smoothness_1: int,
-    surface_smoothness_2: int,
-    cut_off_distance: int,
-) -> napari.qt.threading.FunctionWorker:
-    """Z projection using image interpolation.
-    Args:
-        input_image:
-            Numpy ndarray representation of multi-dimensional image stack.
-        smoothing_radius:
-            Kernel radius for gaussian blur to apply before estimating the surface.
-        surface_smoothness_1:
-            Surface smoothness for 1st griddata estimation, larger means smoother.
-        surface_smoothness_2:
-            Surface smoothness for 3nd griddata estimation, larger means smoother.
-        cut_off_distance:
-            Cutoff distance in z-planes from the 1st estimated surface.
-
-    Returns:
-        Projected image as napari Image layer.
-    """
-
-    if input_image is None:
-        napari.utils.notifications.show_error("Load an image first")
-        return None
-
-    def handle_returned(projection: npt.NDArray[np.float64]) -> None:
-        """Callback for `run` thread worker."""
-
-        projection_widget.viewer = current_viewer()
-        projection_layer = (
-            projection,
-            {"name": PROJECTION_LAYER_NAME},
-            "image",
-        )
-        _add_layers(projection_widget, [projection_layer])
-
-    @thread_worker(connect={"returned": handle_returned})
-    def run() -> npt.NDArray[np.float64]:
-        """Handle clicks on the `Run` button. Runs projection in a
-        separate thread to avoid blocking GUI."""
-
-        return calculate_projection(
-            input_image,
-            smoothing_radius,
-            surface_smoothness_1,
-            surface_smoothness_2,
-            cut_off_distance,
-        )
-
-    return run()
-
-
-def _init_segmentation_widget(
-    container: magicgui.widgets._function_gui.FunctionGui,
-):
-    """Add callbacks for the widget that is used to select the input image"""
-
+    segmentation_widget = napari_epitools.widgets.create_segmentation_widget()
     viewer = napari.current_viewer()
 
     # Automatically select a newly added Image layer
     viewer.layers.events.inserted.connect(
-        lambda event: _select_inserted_image(
+        lambda event: select_inserted_image(
             new_layer=event.value,
-            widget=container.input_image,
+            widget=segmentation_widget.input_image,
         ),
     )
 
+    # Segment the timeseries when pressing the 'Run' button
+    segmentation_widget.run.changed.connect(
+        lambda: run_segmentation(
+            image=segmentation_widget.input_image.value,
+            spot_sigma=segmentation_widget.spot_sigma.value,
+            outline_sigma=segmentation_widget.outline_sigma.value,
+            threshold=segmentation_widget.threshold.value,
+        )
+    )
 
-def _select_inserted_image(
+    return segmentation_widget
+
+
+def select_inserted_image(
     new_layer: napari.layers.Layer,
     widget: magicgui.widgets.ComboBox,
 ):
@@ -220,89 +114,40 @@ def _select_inserted_image(
     widget.native.setCurrentIndex(len(widget) - 1)
 
 
-@magic_factory(
-    spot_sigma=SPOT_SIGMA,
-    outline_sigma=OUTLINE_SIGMA,
-    threshold=THRESHOLD,
-    widget_init=_init_segmentation_widget,
-)
-def segmentation_widget(
-    input_image: napari.types.ImageData,
+def run_segmentation(
+    image: napari.layers.Image,
     spot_sigma: float,
     outline_sigma: float,
     threshold: float,
-) -> napari.qt.threading.FunctionWorker:
-    """Segment cells in a projected image.
+) -> None:
+    """Segment a 3d timeserise (TYZ) at each frame"""
 
-    Args:
-        input_image:
-            Numpy ndarray representation of a projected image stack.
-        spot_sigma:
-            Controls how close segmented cells can be.
-        outline_sigma:
-            Controls how precisely segmented cells are outlined.
-        threshold:
-            Cells with an average intensity below `threshold` are ignored.
-
-    Returns:
-        Seed points as a Napari Points layer and segmented cells as a
-        Napari Labels layer.
-    """
-
-    if input_image is None:
-        napari.utils.notifications.show_error("Load a projection first")
-        return None
-
-    def handle_returned(
-        result: tuple[npt.NDArray[np.float64], npt.NDArray[np.int64]]
-    ) -> None:
-        """Callback for `run` thread worker."""
-
-        segmentation_widget.viewer = current_viewer()
-        seeds, labels = result
-        labels_layer = (labels, {"name": CELLS_LAYER_NAME}, "labels")
-        seeds_layer = (
-            seeds,
-            {
-                "name": SEEDS_LAYER_NAME,
-                "size": SEED_SIZE,
-                "edge_color": SEED_EDGE_COLOR,
-                "face_color": SEED_FACE_COLOR,
-            },
-            "points",
-        )
-        layers = [labels_layer, seeds_layer]
-        _add_layers(segmentation_widget, layers)
-
-    @thread_worker(connect={"returned": handle_returned})
-    def run() -> tuple[npt.NDArray[np.float64], npt.NDArray[np.int64]]:
-        """Handle clicks on the `Run` button. Runs segmentation in a
-        separate thread to avoid blocking GUI.
-        """
-
-        return calculate_segmentation(
-            input_image,
-            spot_sigma,
-            outline_sigma,
-            threshold,
-        )
-
-    return run()
-
-
-def cell_statistics_widget() -> magicgui.widgets.Container:
-    """Create a widget for calculating cell statistics of labelled segmentations."""
-
-    cell_statistics_widgets = _create_cell_statistics_widgets()
-    colour_labels_widgets = _create_colour_labels_widgets()
-
-    cell_statistics_widget = magicgui.widgets.Container(
-        widgets=[*cell_statistics_widgets, *colour_labels_widgets],
-        scrollable=False,
+    seeds_data, labels_data = napari_epitools.analysis.calculate_segmentation(
+        projection=image.data,
+        spot_sigma=spot_sigma,
+        outline_sigma=outline_sigma,
+        threshold=threshold,
     )
 
     viewer = napari.current_viewer()
-    cell_statistics_widget.viewer = viewer
+    viewer.add_labels(
+        data=labels_data,
+        name="Cells",
+    )
+    viewer.add_points(
+        data=seeds_data,
+        name="Seeds",
+        size=3,
+        edge_color="red",
+        face_color="red",
+    )
+
+
+def create_cell_statistics_widget() -> magicgui.widgets.Container:
+    """Create a widget for calculating cell statistics of labelled segmentations."""
+
+    cell_statistics_widget = napari_epitools.widgets.create_cell_statistics_widget()
+    viewer = napari.current_viewer()
 
     # Update cell_statistics when scrolling through frames
     viewer.dims.events.current_step.connect(
@@ -339,132 +184,6 @@ def cell_statistics_widget() -> magicgui.widgets.Container:
     )
 
     return cell_statistics_widget
-
-
-def _create_cell_statistics_widgets() -> list[Widget]:
-    """Create widgets for calculating and exporting cell statistics"""
-
-    image_tooltip = "Select an 'Image' layer to use for calculating cell statistics."
-    image = magicgui.widgets.create_widget(
-        annotation=napari.layers.Image,
-        name="input_image",
-        label="image",
-        options={"tooltip": image_tooltip},
-    )
-
-    labels_tooltip = (
-        "Select a 'Labels' layer to use for calculating cell statistics.\n"
-        "These should be the corresponding labels for the selected Image."
-    )
-    labels = magicgui.widgets.create_widget(
-        annotation=napari.layers.Labels,
-        name="input_labels",
-        label="labels",
-        options={"tooltip": labels_tooltip},
-    )
-
-    run_tooltip = "Calculate cell statistics for the selected Image and Labels"
-    run = magicgui.widgets.create_widget(
-        name="run",
-        label="Calculate statistics",
-        widget_type="PushButton",
-        options={"tooltip": run_tooltip},
-    )
-
-    export_tooltip = (
-        "Export the cell statistics for the selected Image and Labels to a CSV file"
-    )
-    export = magicgui.widgets.create_widget(
-        name="export",
-        label="Export statistics",
-        widget_type="PushButton",
-        options={"tooltip": export_tooltip},
-    )
-
-    return [image, labels, run, export]
-
-
-def _create_colour_labels_widgets() -> list[Widget]:
-    """Create widgets for colouring Labels layers by their featuers"""
-
-    create_colour_labels_heading = magicgui.widgets.create_widget(
-        label="<b>Colour by statistic</b>",  # bold label
-        widget_type="Label",
-        gui_only=True,
-    )
-
-    labels_tooltip = "Select a 'Labels' layer to set colourmap for"
-    labels = magicgui.widgets.create_widget(
-        annotation=napari.layers.Labels,
-        name="colourmap_input_labels",
-        label="labels",
-        options={"tooltip": labels_tooltip},
-    )
-
-    statisitc_tooltip = "Select which statistic to use for colouring the 'Labels'"
-    statistic = magicgui.widgets.create_widget(
-        name="colourmap_statistic",
-        value="id",
-        label="statistic",
-        widget_type="ComboBox",
-        options={
-            "choices": ["id", "area", "perimeter", "orientation", "neighbours"],
-            "tooltip": statisitc_tooltip,
-        },
-    )
-
-    lower_limit_tooltip = "Set the lower limit for the colourmap"
-    lower_limit = magicgui.widgets.create_widget(
-        value=0.0,
-        name="colourmap_lower_limit",
-        label="lower limit",
-        widget_type="FloatSpinBox",
-        options={
-            "tooltip": lower_limit_tooltip,
-            "min": -3.4 * 10**38,
-            "max": 3.4 * 10**38,
-        },
-    )
-
-    upper_limit_tooltip = "Set the upper limit for the colourmap"
-    upper_limit = magicgui.widgets.create_widget(
-        value=100.0,
-        name="colourmap_upper_limit",
-        label="upper limit",
-        widget_type="FloatSpinBox",
-        options={
-            "tooltip": upper_limit_tooltip,
-            "min": -3.4 * 10**38,
-            "max": 3.4 * 10**38,
-        },
-    )
-
-    auto_limits_tooltip = "Automatically set colourmap limits based on feature values."
-    auto_limits = magicgui.widgets.create_widget(
-        value=False,
-        name="colourmap_autolimits",
-        label="Auto-limits",
-        widget_type="CheckBox",
-        options={"tooltip": auto_limits_tooltip},
-    )
-
-    create_colourmap_tooltip = "Create colourmap for the selected Labels layer"
-    create_colourmap = magicgui.widgets.create_widget(
-        name="colourmap_create_button",
-        label="Create colourmap",
-        widget_type="PushButton",
-        options={"tooltip": create_colourmap_tooltip},
-    )
-
-    return [
-        create_colour_labels_heading,
-        labels,
-        statistic,
-        lower_limit,
-        upper_limit,
-        auto_limits,
-        create_colourmap,
-    ]
 
 
 def _update_cell_statistics(
@@ -504,7 +223,7 @@ def run_cell_statistics(
 ) -> None:
     """Calculate cell statistics for all frames in the selected Image and Labels"""
 
-    cell_statistics, graphs = calculate_cell_statistics(
+    cell_statistics, graphs = napari_epitools.analysis.calculate_cell_statistics(
         image=image.data,
         labels=labels.data,
     )
