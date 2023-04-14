b144f7c70bbc5d6e50215420b289bc8837186ab8
Paul Smith <paul.j.smith@ucl.ac.uk>
2023-04-14 17:56:56 +0100

    Move widget creation into a sub-package (#57)
    Fixes #55 
    
    - created `napari_epitools.widgets` sub-package, which currently has
    three modules:
        - `projection`
        - `segmentation`
        - `cell_statistics`
    
    - moved widget creation into these modules
    
    - created a new module named `napari_epitools.main` - defines
    entrypoints for the plugins - created the widgets and defines the
    callbacks
    
    - in `napari_epitools.analysis.projection`, renamed the `IMAGE_NDIM`
    constant to `THREE_DIMENSIONAL` to be more explicit about what its value
    is
    
    - use `magicgui.widgets.create_widgets` for all widget creation to be
    consistent
    
    - temporarily removed use of thread workers - it was making debugging
    difficult as I'm not sure how to use the vs code debugger with the
    thread worker (any breakpoints I added to functions ran by the thread
    worker were ignored)
    
    ---------
    
    Co-authored-by: Patrick Roddy <patrickjamesroddy@gmail.com>

---
+++ src/napari_epitools/widgets/projection.py
@@ -0,0 +1,102 @@
+import magicgui.widgets
+import napari
+
+__all__ = [
+    "create_projection_widget",
+]
+
+
+def create_projection_widget() -> magicgui.widgets.Container:
+    """Create a widget for projecting a 3d timeseries to a 2d timeseries"""
+
+    image_tooltip = "Select an 'Image' layer to project along the z-dimension."
+    image = magicgui.widgets.create_widget(
+        annotation=napari.layers.Image,
+        name="input_image",
+        label="image",
+        options={"tooltip": image_tooltip},
+    )
+
+    smoothing_radius_tooltip = (
+        "Kernel radius for gaussian blur to apply before estimating the surface."
+    )
+    smoothing_radius = magicgui.widgets.create_widget(
+        value=1,
+        name="smoothing_radius",
+        label="smoothing radius",
+        widget_type="FloatSlider",
+        options={
+            "min": 0,
+            "max": 10,
+            "step": 0.1,
+            "tooltip": smoothing_radius_tooltip,
+        },
+    )
+
+    surface_smoothness_1_tooltip = (
+        "Surface smoothness for 1st griddata estimation, larger means smoother."
+    )
+    surface_smoothness_1 = magicgui.widgets.create_widget(
+        value=5,
+        name="surface_smoothness_1",
+        label="surface smoothness 1",
+        widget_type="Slider",
+        options={
+            "min": 0,
+            "max": 10,
+            "step": 1,
+            "tooltip": surface_smoothness_1_tooltip,
+        },
+    )
+
+    surface_smoothness_2_tooltip = (
+        "Surface smoothness for 2nd griddata estimation, larger means smoother."
+    )
+    surface_smoothness_2 = magicgui.widgets.create_widget(
+        value=5,
+        name="surface_smoothness_2",
+        label="surface smoothness 2",
+        widget_type="Slider",
+        options={
+            "min": 0,
+            "max": 10,
+            "step": 1,
+            "tooltip": surface_smoothness_2_tooltip,
+        },
+    )
+
+    cutoff_distance_tooltip = (
+        "Cutoff distance in z-planes from the 1st estimated surface."
+    )
+    cutoff_distance = magicgui.widgets.create_widget(
+        value=3,
+        name="cutoff_distance",
+        label="z cutoff distance",
+        widget_type="Slider",
+        options={
+            "min": 0,
+            "max": 5,
+            "step": 1,
+            "tooltip": cutoff_distance_tooltip,
+        },
+    )
+
+    run_button_tooltip = "Perform the projection"
+    run_button = magicgui.widgets.create_widget(
+        name="run",
+        label="Run",
+        widget_type="PushButton",
+        options={"tooltip": run_button_tooltip},
+    )
+
+    return magicgui.widgets.Container(
+        widgets=[
+            image,
+            smoothing_radius,
+            surface_smoothness_1,
+            surface_smoothness_2,
+            cutoff_distance,
+            run_button,
+        ],
+        scrollable=False,
+    )
---
+++ src/napari_epitools/widgets/segmentation.py
@@ -0,0 +1,79 @@
+import magicgui.widgets
+import napari
+
+__all__ = [
+    "create_segmentation_widget",
+]
+
+
+def create_segmentation_widget() -> magicgui.widgets.Container:
+    """Create a widget for segmenting cells in a 3d (TYZ) image"""
+
+    image_tooltip = "Select an 'Image' layer to segment"
+    image = magicgui.widgets.create_widget(
+        annotation=napari.layers.Image,
+        name="input_image",
+        label="image",
+        options={"tooltip": image_tooltip},
+    )
+
+    spot_sigma_tooltip = "Controls how close segmented cells can be."
+    spot_sigma = magicgui.widgets.create_widget(
+        value=10.0,
+        name="spot_sigma",
+        label="spot sigma",
+        widget_type="FloatSlider",
+        options={
+            "min": 0,
+            "max": 20,
+            "step": 0.1,
+            "tooltip": spot_sigma_tooltip,
+        },
+    )
+
+    outline_sigma_tooltip = "Controls how precisely segmented cells are outlined."
+    outline_sigma = magicgui.widgets.create_widget(
+        value=3.0,
+        name="outline_sigma",
+        label="outline sigma",
+        widget_type="FloatSlider",
+        options={
+            "min": 0,
+            "max": 20,
+            "step": 0.1,
+            "tooltip": outline_sigma_tooltip,
+        },
+    )
+
+    threshold_tooltip = "Cells with an average intensity below `threshold` are ignored."
+    threshold = magicgui.widgets.create_widget(
+        value=3.0,
+        name="threshold",
+        label="threshold sigma",
+        widget_type="FloatSlider",
+        options={
+            "min": 0,
+            "max": 100,
+            "step": 0.5,
+            "tooltip": threshold_tooltip,
+        },
+    )
+
+    run_button_tooltip = "Perform the segmentation"
+    run_button = magicgui.widgets.create_widget(
+        name="run",
+        label="Run",
+        widget_type="PushButton",
+        options={"tooltip": run_button_tooltip},
+    )
+
+    return magicgui.widgets.Container(
+        widgets=[
+            image,
+            spot_sigma,
+            outline_sigma,
+            threshold,
+            run_button,
+        ],
+        scrollable=False,
+    )
