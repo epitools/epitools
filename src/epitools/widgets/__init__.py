a3bda2786f645582031d1ad2bfb43d0ec8167a20
Paul Smith <paul.j.smith@ucl.ac.uk>
2023-04-25 14:18:09 +0100

    Add function to luanch diagluge with combobox for user selection

--- src/epitools/widgets/__init__.py
+++ src/epitools/widgets/__init__.py
@@ -1,3 +1,4 @@
+from napari_epitools.widgets import dialogue
 from epitools.widgets.cell_statistics import create_cell_statistics_widget
 from epitools.widgets.projection import create_projection_widget
 from epitools.widgets.segmentation import create_segmentation_widget
