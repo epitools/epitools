99f1bd99d54333cde15ba9a3b98767ba5f57fe58
Paul Smith <paul.j.smith@ucl.ac.uk>
2023-04-25 14:24:40 +0100

    Add initial code for user to selected axes order when saving a file

--- src/epitools/_writer.py
+++ src/epitools/_writer.py
@@ -11,7 +11,16 @@ from __future__ import annotations
 import pathlib
 from typing import Any
 
-import PartSegCore.napari_plugins.save_tiff_layer
+import PartSegImage
+
+SCALE_MASKS = {
+    "XY": [0, 1],  # 2D grayscale
+    "TYX": [1, 2],  # 2D grayscale timeseries
+    "TYXC": [1, 2],  # 2D multichannel timeseries
+    "ZYX": [0, 1, 2],  # 3D grayscale
+    "TZYX": [1, 2, 3],  # 3D grayscale timeseries
+    "TZYXC": [1, 2, 3],  # 3D multichannel timeseries
+}
 
 
 def write_single_image(path: str | pathlib.Path, data: Any, meta: dict):
@@ -22,24 +31,61 @@ def write_single_image(path: str | pathlib.Path, data: Any, meta: dict):
         path = path.with_suffix(".tif")
 
     if path.suffix not in [".tif", ".tiff"]:
-        return
+        return []
 
-    path = PartSegCore.napari_plugins.save_tiff_layer.napari_write_image(
-        path=path.as_posix(),
+    # Undo scaling before saving
+    scale_factor = PartSegImage.Image.DEFAULT_SCALE_FACTOR
+    scale_shift = min(data.ndim, 3)
+
+    # TODO: launch dialogue for user to select axes order
+    axes = "TZYX"
+    channel_names = [meta["name"]]
+    if data.shape[-1] < 6:  # noqa: PLR2004
+        axes += "C"
+        scale_shift -= 1
+        channel_names = [f'{meta["name"]} {i}' for i in range(1, data.shape[-1] + 1)]
+
+    image = PartSegImage.Image(
         data=data,
-        meta=meta,
+        image_spacing=(meta["scale"] / scale_factor)[-scale_shift:],
+        axes_order="TZXY"[-data.ndim :],
+        channel_names=channel_names,
+        shift=(meta["translate"] / scale_factor)[-scale_shift:],
+        name="ROI",
+    )
+    PartSegImage.ImageWriter.save(
+        image=image,
+        path=path.as_posix(),
     )
 
-    return path
+    return [path]
 
 
-def write_single_labels(path: str, data: Any, meta: dict):
+def write_single_labels(path: str | pathlib.Path, data: Any, meta: dict):
     """Writes a single labels layer"""
 
-    path = PartSegCore.napari_plugins.save_tiff_layer.napari_write_labels(
-        path=path,
+    path = pathlib.Path(path)
+    if not path.suffix:
+        path = path.with_suffix(".tif")
+
+    if path.suffix not in [".tif", ".tiff"]:
+        return []
+
+    # Undo scaling before saving
+    scale_factor = PartSegImage.Image.DEFAULT_SCALE_FACTOR
+    scale_shift = min(data.ndim, 3)
+
+    image = PartSegImage.Image(
         data=data,
-        meta=meta,
+        image_spacing=(meta["scale"] / scale_factor)[-scale_shift:],
+        axes_order="TZXY"[-data.ndim :],
+        channel_names=[meta["name"]],
+        shift=(meta["translate"] / scale_factor)[-scale_shift:],
+        name="ROI",
+    )
+    PartSegImage.ImageWriter.save(
+        image=image,
+        path=path.as_posix(),
     )
 
-    return path
+    return [path]
