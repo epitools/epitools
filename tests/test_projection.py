04a09994de36b759e721b0809fbaddce5afaeb22
Paul Smith <paul.j.smith@ucl.ac.uk>
2023-05-03 11:59:27 +0100

    Patch the same function we use in epitools.main for projecting data

--- tests/test_projection.py
+++ tests/test_projection.py
@@ -16,22 +16,22 @@ PROJECTION_NDIM = 3
 
 
 @pytest.fixture(scope="function")
-def test_image() -> napari.layers.Image:
+def image() -> napari.layers.Image:
     data, metadata, layer_type = epitools._sample_data.load_sample_data()[0]
     metadata["name"] = "Test Image"
     return napari.layers.Image(data, **metadata)
 
 
 @pytest.fixture(scope="function")
-def test_projected_image() -> napari.layers.Image:
+def projected_image() -> napari.layers.Image:
     data, metadata, layer_type = epitools._sample_data.load_projected_data()[0]
     return napari.layers.Image(data, **metadata)
 
 
 @pytest.fixture(scope="function")
-def viewer_with_test_image(make_napari_viewer, test_image) -> napari.Viewer:
+def viewer_with_image(make_napari_viewer, image) -> napari.Viewer:
     viewer = make_napari_viewer()
-    viewer.add_layer(test_image)
+    viewer.add_layer(image)
 
     return viewer
 
@@ -50,39 +50,35 @@ def test_add_projection_widget(make_napari_viewer):
 
 
 def test_projection_widget_run_button(
-    viewer_with_test_image,
-    test_projected_image,
+    viewer_with_image,
+    projected_image,
 ):
-    dock_widget, container = viewer_with_test_image.window.add_plugin_dock_widget(
+    dock_widget, container = viewer_with_image.window.add_plugin_dock_widget(
         plugin_name="epitools",
         widget_name="Projection (selective plane)",
     )
 
     # use saved image data so we don't run the projection analysis
     # when the button is pressed
-    with patch(
-        "epitools.analysis.projection.calculate_projection"
-    ) as calculate_projection:
-        calculate_projection.return_value = test_projected_image.data
+    with patch("epitools.analysis.calculate_projection") as calculate_projection:
+        calculate_projection.return_value = projected_image.data
         container.run.clicked()
 
-    assert len(viewer_with_test_image.layers) == 2  # noqa: PLR2004
+    assert len(viewer_with_image.layers) == 2  # noqa: PLR2004
 
-    original_layer, new_layer = viewer_with_test_image.layers
+    original_layer, new_layer = viewer_with_image.layers
 
     assert isinstance(new_layer, napari.layers.Image)
     assert new_layer.name == "Projection"
     assert new_layer.data.shape[-2:] == original_layer.data.shape[-2:]  # yx dimensions
 
 
-def test_calculate_projection(test_image):
+def test_calculate_projection(image):
     with patch("epitools.analysis.projection._interpolate") as interp:
-        mock_interpolation = np.zeros(
-            (test_image.data.shape[2], test_image.data.shape[3])
-        )
+        mock_interpolation = np.zeros((image.data.shape[2], image.data.shape[3]))
         interp.return_value = mock_interpolation
         projection = calculate_projection(
-            test_image.data,
+            image.data,
             SMOOTHING_RADIUS,
             SURFACE_SMOOTHNESS_1,
             SURFACE_SMOOTHNESS_2,
@@ -91,6 +87,6 @@ def test_calculate_projection(test_image):
         assert projection.ndim == PROJECTION_NDIM
         assert projection.shape == (
             1,  # single frame in the timeseries
-            test_image.data.shape[2],
-            test_image.data.shape[3],
+            image.data.shape[2],
+            image.data.shape[3],
         )
