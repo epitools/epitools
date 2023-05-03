99efed7e74143237a0044168166fcdcafb6db45e
Paul Smith <paul.j.smith@ucl.ac.uk>
2023-05-03 11:43:34 +0100

    Add functions to load sample projected and segmented data

--- src/epitools/_sample_data.py
+++ src/epitools/_sample_data.py
@@ -10,11 +10,29 @@ from __future__ import annotations
 
 from pathlib import Path
 
+import napari
+
 import epitools._reader
 
 
-def load_sample_data():
+def load_sample_data() -> napari.layers.Image:
     """Load a sample dataset"""
 
     img_path = Path("sample_data") / "8bitDataset" / "test_image.tif"
+    return epitools._reader.reader_function(path=img_path.as_posix())
+
+
+def load_projected_data() -> napari.layers.Image:
+    """Load a smaple projected dataset"""
+
+    img_path = Path("sample_data") / "8bitDataset" / "test_image-projected.tif"
+    return epitools._reader.reader_function(path=img_path.as_posix())
+
+
+def load_segmented_data() -> napari.layers.Label:
+    """Load a sample segmented dataset"""
+
+    img_path = (
+        Path("sample_data") / "8bitDataset" / "test_image-projected-segmented.tif"
+    )
     return epitools._reader.reader_function(path=img_path.as_posix())
